ca0f7eb362f6d8de3f1ec59b1f6831971c84a1c0
Gustavo Oliveira <gustavo.dev@outlook.com.br>
2019-05-21 09:29:07 -0300

    Novo arquivo de input no projeto dotnet

---
+++ dotnet/SP500HoltWinters/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using TinyCsvParser;
+
+namespace SP500HoltWinters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parser = new CsvParser<ItemLinha>(
+                new CsvParserOptions(true, ';'),
+                new ItemLinhaMapper());
+
+            var linhas = parser
+                .ReadFromFile("sp500input.csv", Encoding.UTF8)
+                .Select(x => x.Result)
+                .ToList();
+
+            var calculator = new HoltWinterCalculator(linhas, 90);
+            var result = calculator.Calculate(0);
+
+            using (var file = File.CreateText("HoltWinters.csv"))
+            {
+                file.WriteLine($"Data;Valor;L;B;S;F;E;E%");
+
+                foreach (var arr in result)
+                {
+                    file.WriteLine(string.Join(";", arr));
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
