9a4ac86f24679967a0431c174046a93b3aba6ace
Gustavo Santos <gustavo.dev@outlook.com.br>
2019-05-22 19:08:05 -0300

    Refatoração da lógica de csv para a classe gerenciadora de calculos

--- python/holtwinters.py
+++ python/holtwinters.py
@@ -1,3 +1,15 @@
+import pandas as pd
+import datetime
+from item import Item
+
 class HoltWinters:
-    def __init__(self):
-        print('Teste')
+    def __init__(self, itens):
+        self.itens = itens
+
+    @staticmethod
+    def fromcsv(filePath):
+        data = pd.read_csv(filePath, sep = ';')
+        data = map(lambda i: Item(datetime.datetime.strptime(i[0], '%d/%m/%Y'), i[1]), data.values.tolist())
+        data = list(data)
+        data.sort(key= lambda x: x.data)
+        return HoltWinters(data)
