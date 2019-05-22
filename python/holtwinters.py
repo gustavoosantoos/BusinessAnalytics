3d2e566c853e3c2d3d0a0b42017e88744bdb178f
Gustavo Santos <gustavo.dev@outlook.com.br>
2019-05-22 19:59:52 -0300

    Setup holtwinters

--- python/holtwinters.py
+++ python/holtwinters.py
@@ -1,15 +1,28 @@
 import pandas as pd
 import datetime
 from item import Item
+from functools import reduce
 
 class HoltWinters:
-    def __init__(self, itens):
+    def __init__(self, itens, tamanhoPeriodo = 3):
         self.itens = itens
+        self.__alfa__ = 0.8
+        self.__beta__ = 0.8
+        self.__gama__ = 0.8
+        self.__tamanhoPeriodo__ = tamanhoPeriodo
+
+    def __setup__(self): 
+        primeiroPeriodo = self.itens[0:self.__tamanhoPeriodo__]
+        for item in primeiroPeriodo:
+            item.s = item.valor / (reduce(lambda a, b: a + b.valor, primeiroPeriodo, 0) / self.__tamanhoPeriodo__)
+
+    def calculate(self, numeroPrevisoes):
+        self.__setup__()
 
     @staticmethod
-    def fromcsv(filePath):
+    def fromcsv(filePath, tamanhoPeriodo):
         data = pd.read_csv(filePath, sep = ';')
         data = map(lambda i: Item(datetime.datetime.strptime(i[0], '%d/%m/%Y'), i[1]), data.values.tolist())
         data = list(data)
         data.sort(key= lambda x: x.data)
-        return HoltWinters(data)
+        return HoltWinters(data, tamanhoPeriodo)
