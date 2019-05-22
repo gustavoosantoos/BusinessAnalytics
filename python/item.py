9bea3cd9385d1c25e80617cd53acf0c1b6029ddf
Gustavo Santos <gustavo.dev@outlook.com.br>
2019-05-22 19:14:36 -0300

    Adição de campos HoltWinters na classe item

--- python/item.py
+++ python/item.py
@@ -2,3 +2,10 @@ class Item:
     def __init__(self, data, valor):
         self.data = data
         self.valor = valor
+    
+    l = None
+    b = None
+    s = None
+    f = None
+    e = None
+    eperc = None
